6a441a5668c59c429debe4242e8c7a75a938b1e3
77Fayy <Fay_3bdulrahman@hotmail.com>
2023-10-08 11:13:12 +0300

    Initial Commit

---
+++ Ringo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Ringo
+//
+//  Created by fai alradhi on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        Image("Image1")
+                
+        Text("Fai Alradhi")
+            Text("Software engineer interested in iOS and Ui/Ux ")
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ringo/RingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingoApp.swift
+//  Ringo
+//
+//  Created by fai alradhi on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct RingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
